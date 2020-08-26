3d9b8dd20ee90e994e69223374fe92da125b8c55
energetic-certificate-0804 <mjbugay@yahoo.com>
2020-08-26 01:45:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,5 @@
 def display_rainbow(x)
   puts "R: #{x[0]}, O: #{x[1]}, Y: #{x[2]}, G: #{x[3]}, B: #{x[4]}, I: #{x[5]}, V: #{x[6]}
 end
-rainbow = 
+rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+puts display_rainbow(rainbow)
